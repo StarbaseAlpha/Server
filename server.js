051660fda4d4f49db6fdb77ff420fabdd7ab60c8
Mike Fuller <emailthemike@gmail.com>
2019-10-21 22:30:01 -0400

    major update: no longer uses Defacto package

--- server.js
+++ server.js
@@ -1,11 +1,45 @@
 'use strict';
   
-const Defacto = require('@starbase/defacto');
 const theRules = require('@starbase/therules');
+const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
+const cors = require('cors');
 
-function Server(port, options) {
+function Server(port=null,options={"staticPath":null,"jsonLimit":1024*1024*1,"maxAge":1000 * 10,"cors":null}) {
 
-  const {server, app} = Defacto(port, options);
+  if (!port || typeof port !== 'number') {
+    let err = new Error('Missing port!');
+    throw(err);
+    return null;
+  }
+
+  const app = express();
+
+  app.use(compression());
+  app.use(helmet());
+
+  if (options.cors && typeof options.cors === 'object') {
+    app.use(cors(options.cors));
+  }
+  if (options.staticPath && typeof options.staticPath === 'string') {
+    app.use(express.static(options.staticPath,{"maxAge":(options.maxAge||1000 * 10)}));
+  }
+
+  app.use(express.json({
+    "limit":(options.jsonLimit || 1024 * 1024 * 1)
+  }));
+
+  app.use((err,req,res,next)=>{
+    res.status(400).json({"code":400,"message":err.message||err.toString()});
+  });
+
+  const server = app.listen(port,(err)=>{
+    if (err) {
+      throw(err);
+      return null;
+    }
+  });
 
   const api = (endpoint, options={"rules":[], "kit":{}}) => {
 
